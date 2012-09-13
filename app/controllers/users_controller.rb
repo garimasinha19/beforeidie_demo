ba8c15aba77860011c7bd0641271ae7b6068f31f
garimasinha19 <garimasinha19@gmail.com>
2012-09-13 10:44:44 -0700

    finish user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,22 @@ class UsersController < ApplicationController
 
   
   def new
+  	@user = User.new
+  end
+
+  
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+  		flash[:success] = "Welcome to before I die!"
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end	
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
 end
